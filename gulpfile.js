7da2b5a87184549097cca4ff93730040b8a45e48
Himanshu Bisht <Himanshubisht227@gmail.com>
2018-05-17 13:37:51 -0500

    babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 let cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
 
  
 gulp.task('css', () =>
@@ -19,4 +20,13 @@ gulp.task('img', () =>
     gulp.src('src/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/images'))
+);
+
+ 
+gulp.task('js', () =>
+    gulp.src('src/js/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('dist'))
 );
